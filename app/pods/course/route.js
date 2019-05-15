aa78cf58f5cb5b6a503a5d83c7adc325891c1f47
Reggie Bigornia <reggietheroman@gmail.com>
2019-05-15 19:37:08 +0800

    Updated titles for the main routes and courses pages only.

--- app/pods/course/route.js
+++ app/pods/course/route.js
@@ -1,6 +1,9 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  titleToken: function(model) {
+    return model.firstObject.title;
+  },
   model({ course }) {
     return this.get('store').query('category', { slug: course, include: ['subCategories']});
   }
