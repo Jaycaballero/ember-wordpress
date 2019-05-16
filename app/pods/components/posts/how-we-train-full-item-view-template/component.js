0d5973d52beb6bbe3f0ea8e336e861ae78de2eed
Jay Caallero <truthofmyyouth12345@gmail.com>
2019-05-16 16:26:40 +0800

    update layout for how we train pages

--- app/pods/components/posts/how-we-train-full-item-view-template/component.js
+++ app/pods/components/posts/how-we-train-full-item-view-template/component.js
@@ -6,8 +6,9 @@ import { htmlSafe } from '@ember/string';
 export default Component.extend({
   fullItemViewContent: map('_full_Item_View', function(fullItemViewContent){
     return{
+      paragraphLead: htmlSafe(get(fullItemViewContent, 'acf.paragraph_lead')),
       title: htmlSafe(get(fullItemViewContent, 'title')),
-      content: htmlSafe(get(fullItemViewContent, 'content')),
+      content: htmlSafe(get(fullItemViewContent, 'acf.editor')),
       image: get(fullItemViewContent, 'better_featured_image.source_url')
     }
   }),
