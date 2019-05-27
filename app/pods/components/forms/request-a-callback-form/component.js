fb80ca4ef52b460e1c24bf36920d21c9a9573ef1
Reggie Bigornia <reggietheroman@gmail.com>
2019-05-27 20:20:28 +0800

    Updated source and source description for forms for leads.

--- app/pods/components/forms/request-a-callback-form/component.js
+++ app/pods/components/forms/request-a-callback-form/component.js
@@ -4,7 +4,8 @@ import { get, set } from '@ember/object';
 export default Component.extend({
   actions: {
     submit() {
-      set(this, 'lead.source', `Skill: ${get(this, 'subCategoryName')}`);
+      set(this, 'lead.source', 'Request a Callback');
+      set(this, 'lead.sourceDescription', `Skill: ${get(this, 'subCategoryName')}`);
       return get(this, 'onSubmit')(get(this, 'lead'));
     }
   }
