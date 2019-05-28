06010fcc0b13c4eafd5d2ca2f5a7ef46d0a2a049
Reggie Bigornia <reggietheroman@gmail.com>
2019-05-28 19:40:42 +0800

    Updated routes to use model for parent routs to prevent reloading data, causing the lists to reorder themselves.

--- app/pods/course/sub-category/route.js
+++ app/pods/course/sub-category/route.js
@@ -6,7 +6,7 @@ export default Route.extend({
   model({sub_category_slug}) {
     return RSVP.hash({
       testimonials: this.store.query('clienttestimonial', {per_page: 1, order: 'desc'}),
-      subCategories: this.get('store').query('sub-category', { slug: sub_category_slug, include: ['courses'] }),
+      subCategorySlug: sub_category_slug,
       crmLead: get(this, 'store').createRecord('crm-lead', {}),
     });
   },
