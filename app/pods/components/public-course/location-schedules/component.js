1c8f6870caaa4dc4988565afbc861c387ae01e0e
Jay Caallero <truthofmyyouth12345@gmail.com>
2019-07-01 12:41:03 +0800

    adding locations column inside the booking location lists

--- app/pods/components/public-course/location-schedules/component.js
+++ app/pods/components/public-course/location-schedules/component.js
@@ -1,7 +1,18 @@
 import Component from '@ember/component';
+import { map } from '@ember/object/computed';
+import { A } from '@ember/array';
 
 export default Component.extend({
   elementId: 'booking-datas',
   classNames: 'py-5',
-  tagName: 'section'
+  tagName: 'section',
+
+  bookings: map('location.acf.details', function(detail) {
+    return {
+      date: get(detail, 'date'),
+      locationTitle: get(detail, 'location.title'),
+      price: get(detail, 'price')
+    }
+  }),
+
 });
